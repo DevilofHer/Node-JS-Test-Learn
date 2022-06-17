13e5fc26644904f0bf13b0213072ec2052c40942
DevilofHer <107661354+DevilofHer@users.noreply.github.com>
2022-06-17 09:23:45 +0700

    Add all files of Project
    Test code :100:

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) =>{
+
+    res.send('hello test nodejs')
+})
+
+
+app.listen(port, ()=>{
+
+    debug("listening on port"+ chalk.green(" : "+port));
+
+})
+
